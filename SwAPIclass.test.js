118ce1c50634c9330765f5ead99ff0e148cbff18
Daniel <daniel.bencz@gmail.com>
2020-04-06 08:52:18 +0200

    comments

--- SwAPIclass.test.js
+++ SwAPIclass.test.js
@@ -2,23 +2,26 @@
 
 import SwAPI from './SwAPIclass'
 
+//Arrange
 let api
 
 beforeEach(() => {
   let mockRes = {
     results: ['Luke', 'Leia', 'Han']
   }
-  
+
   let customFetch = jest.fn().mockResolvedValue({
     json: jest.fn().mockResolvedValue(mockRes)
   })
-  
+
   //api = new SwAPI(fetch)  // this is the global.fetch defined in setupTests
   api = new SwAPI(customFetch)
 })
 
 test('getPerson returns non-null result', async () => {
- let result = await api.getPerson(1)
+  //Act
+  let result = await api.getPerson(1)
 
+  //Assert
   expect(result).not.toBeNull()
 })
